b2855b2c6f8da25f95786863c5a3feef9ba48137
almrttsn <88113243+almrttsn@users.noreply.github.com>
2022-05-20 18:11:30 +0300

    calculator implemented

--- Assets/Scripts/GiraffeAcademy/Giraffe.cs
+++ Assets/Scripts/GiraffeAcademy/Giraffe.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Giraffe : MonoBehaviour
 {
+    [SerializeField] private int num1;
+    [SerializeField] private int num2;
+    [SerializeField] private string op;
+
     private bool _isVeysi = true;
     private bool _isGoodMan = true;
+    float calculationResult;
+
+
 
     private void Start()
     {
         VeysiOrNot();
+        Debug.Log(GetMax(num1, num2));
+        Calculator();
+        Debug.Log(calculationResult);
     }
 
     private void VeysiOrNot()
@@ -31,4 +41,31 @@ public class Giraffe : MonoBehaviour
             Debug.Log("Shame on you!");
         }
     }
+
+    private int GetMax(int num1, int num2)
+    {
+        int result;
+        if(num1 > num2)
+        {
+            result = num1;
+        }
+        else
+        {
+            result = num2;
+        }
+        return result;
+    }
+
+    private void Calculator()
+    {
+        if(op == "+")
+        {
+            calculationResult = num1 + num2;
+        }
+        else if(op == "-")
+        {
+            calculationResult = num1 - num2;
+        }
+    }
+    
 }
